7d7163ffbc18bf6fc89f76e822a4633f6ba4ab3c
Alex Ilin <alex.a.ilin@gmail.com>
2007-10-01 08:07:51 +0000

    added datetime

---
+++ sources/AnjLab.FX/Datetime/DateUtils.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace AnjLab.FX.Datetime
+{
+    public class DateUtils
+    {
+        public static int DaysInYear(DateTime date)
+        {
+            return DaysInYear(date.Year);
+        }
+
+        public static int DaysInYear(int year)
+        {
+            return DateTime.IsLeapYear(year) ? 366 : 365;
+        }
+
+        public static DateTime YearFirstDay(DateTime year)
+        {
+            return new DateTime(year.Year, 1, 1);
+        }
+
+        public static DateTime YearLastDay(DateTime year)
+        {
+            return new DateTime(year.Year, 12, 31);
+        }
+    }
+}
