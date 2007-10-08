3ab7202e8433070122a0ee3722003f9f2f646fd4
Alex Ilin <alex.a.ilin@gmail.com>
2007-10-08 15:14:35 +0000

    - enhance skipper speed. part 2

--- sources/AnjLab.FX/System/AttributeSearcher.cs
+++ sources/AnjLab.FX/System/AttributeSearcher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -6,9 +7,7 @@ namespace AnjLab.FX.System
 {
     public class AttributeSearcher
     {
-        const BindingFlags AllMembers =
-            BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
-
+        const BindingFlags AllMembers = BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
         const BindingFlags DeclaredOnlyMembers = AllMembers | BindingFlags.DeclaredOnly;
 
         public static IList<TAttribute> GetMemberAttributes<TAttribute>(Type type)
@@ -42,7 +41,48 @@ namespace AnjLab.FX.System
         public static TAttribute GetAttribute<TAttribute>(MemberInfo minfo)
            where TAttribute : Attribute
         {
-            return Attribute.GetCustomAttribute(minfo, typeof(TAttribute)) as TAttribute;
+            long key = GetCacheKey(typeof(TAttribute), minfo);
+
+            if (ContainsInCache(key))
+                return GetFromCache<TAttribute>(key);
+
+            lock (_cache)
+            {
+                if (ContainsInCache(key))
+                    return GetFromCache<TAttribute>(key);
+
+                TAttribute attr = Attribute.GetCustomAttribute(minfo, typeof (TAttribute)) as TAttribute;
+                AddToCache(key, attr);
+                return attr;
+            }
         }
+
+        private static void AddToCache<TAttribute>(long key, TAttribute atr)
+             where TAttribute : Attribute
+        {
+            _cache.Add(key, atr);
+        }
+
+        private static TAttribute GetFromCache<TAttribute>(long key)
+             where TAttribute : Attribute
+        {
+            return _cache[key] as TAttribute;
+        }
+
+        private static bool ContainsInCache(long key)
+        {
+            return _cache.ContainsKey(key);
+        }
+
+        private static long GetCacheKey(Type type, MemberInfo minfo)
+        {
+            long key;
+            key = minfo.MetadataToken;
+            key = key << 32;
+            key += type.GetHashCode();
+            return key;
+        }
+
+        private static Hashtable _cache = new Hashtable();
     }
 }
