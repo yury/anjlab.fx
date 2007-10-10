002ce5b27a93506692b19d151ca919a4e5fbd35e
Alex Ilin <alex.a.ilin@gmail.com>
2007-10-10 12:02:50 +0000

    - price percent can be less then 0 (for old code)

--- sources/AnjLab.FX/Finance/Yield.cs
+++ sources/AnjLab.FX/Finance/Yield.cs
@@ -10,8 +10,7 @@ namespace AnjLab.FX.Finance
         {
             Guard.ArgumentNotNull("currentDate", currentDate);
             Guard.ArgumentNotNull("futureDate", futureDate);
-            Guard.ArgumentGreaterThenZero("pricePercent", pricePercent);
-
+            
             return (100*(100 - pricePercent))/(GetTimeInterval(currentDate, futureDate)*pricePercent);
         }
 
