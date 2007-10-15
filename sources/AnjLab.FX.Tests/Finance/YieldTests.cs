30809b128187bf4ecec9c630248c0d61a1c27400
Alex Ilin <alex.a.ilin@gmail.com>
2007-10-15 13:28:54 +0000

    - unit tests

--- sources/AnjLab.FX.Tests/Finance/YieldTests.cs
+++ sources/AnjLab.FX.Tests/Finance/YieldTests.cs
@@ -13,6 +13,8 @@ namespace AnjLab.FX.Finance
             Assert.AreEqual(52.1368, Math.Round(Yield.Calculate(DateTime.Parse("05.09.2008"), DateTime.Parse("22.11.2008"), 90), 4));
             Assert.AreEqual(2.009, Math.Round(Yield.Calculate(DateTime.Parse("03.10.2003"), DateTime.Parse("30.10.2005"), 96), 4));
             Assert.AreEqual(1.1042, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999"), DateTime.Parse("25.10.2009"), 90), 4));
+
+            Assert.AreEqual(0, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999"), DateTime.Parse("02.10.1999"), 90), 4));
         }
     }
 }
