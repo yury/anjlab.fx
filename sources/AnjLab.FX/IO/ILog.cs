a5f2079b4ce7db434a7a74598cebdf4a0ae31c27
Yury Korolev <yury.korolev@gmail.com>
2007-10-15 16:19:42 +0000

    added ILog

---
+++ sources/AnjLab.FX/IO/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnjLab.FX.IO
+{
+    public interface ILog
+    {
+        void Info(string message, params string[] args);
+        void Warning(string message, params string[] args);
+        void Error(string message, params string[] args);
+        void Fatal(string message, params string[] args);
+    }
+}
---
+++ sources/AnjLab.FX/IO/TraceLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AnjLab.FX.IO
+{
+    public class TraceLog: ILog
+    {
+        public void Info(string message, params string[] args)
+        {
+            Trace.WriteLine(string.Format(message, args), "Info");
+        }
+
+        public void Warning(string message, params string[] args)
+        {
+            Trace.WriteLine(string.Format(message, args), "Warning");
+        }
+
+        public void Error(string message, params string[] args)
+        {
+            Trace.WriteLine(string.Format(message, args), "Error");
+        }
+
+        public void Fatal(string message, params string[] args)
+        {
+            Trace.WriteLine(string.Format(message, args), "Fatal");
+        }
+    }
+}
