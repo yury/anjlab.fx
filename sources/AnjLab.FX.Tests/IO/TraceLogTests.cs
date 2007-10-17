222c5a4264036970c5eb00f6c80a4cb29c1d409d
Yury Korolev <yury.korolev@gmail.com>
2007-10-17 12:29:52 +0000

    added some tests to TraceLog

---
+++ sources/AnjLab.FX.Tests/IO/TraceLogTests.cs
@@ -0,0 +1,19 @@
+﻿using AnjLab.FX.IO;
+using NUnit.Framework;
+
+namespace AnjLab.FX.Tests.IO
+{
+    [TestFixture]
+    public class TraceLogTests
+    {
+        [Test]
+        public void Test()
+        {
+            TraceLog log = new TraceLog();
+            log.Info("test info {0}", 1);
+            log.Warning("test warning {0}", 2);
+            log.Error("test error {0}", 3);
+            log.Fatal("test fatal {0}", 4);
+        }
+    }
+}
