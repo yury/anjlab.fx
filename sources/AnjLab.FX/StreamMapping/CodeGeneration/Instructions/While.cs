b1f06168c02e21a9c8650b316e3921e674386a31
Alex Ilin <alex.a.ilin@gmail.com>
2007-10-30 09:42:50 +0000

    - added while loop into stream mapping

---
+++ sources/AnjLab.FX/StreamMapping/CodeGeneration/Instructions/While.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom;
+using AnjLab.FX.StreamMapping.CodeGeneration;
+using AnjLab.FX.System;
+
+namespace AnjLab.FX.StreamMapping.Instructions
+{
+    public class While : ContainerMapElement
+    {
+        private ICondition _condition;
+
+        public override void GenerateMappingCode(CodeGenerationContext ctx, CodeMemberMethod method)
+        {
+            Guard.NotNull(_condition, "While property can't be null in Do mapping");
+            //bool conditionResult = condition;
+            //while(condition)
+            //{
+            //    // code
+            //    conditionResult = condition;
+            //}
+            CodeExpression condition = _condition.GetCondition(ctx, method);
+            method.Statements.Add(new CodeVariableDeclarationStatement(
+                typeof(bool), "conditionResult", condition));
+            CodeVariableReferenceExpression conditionResult = new CodeVariableReferenceExpression("conditionResult");
+
+            method.Statements.Add(new CodeSnippetStatement("while (conditionResult) {"));
+            
+            base.GenerateMappingCode(ctx, method);
+
+            method.Statements.Add(new CodeAssignStatement(conditionResult, condition));
+            method.Statements.Add(new CodeSnippetStatement("}"));
+        }
+
+        public ICondition Condition
+        {
+            get { return _condition; }
+            set { _condition = value; }
+        }
+    }
+}
