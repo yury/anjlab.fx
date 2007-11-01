1c4223aa47774570649dd33a39818ccf08e807b3
Alex Ilin <alex.a.ilin@gmail.com>
2007-11-01 17:17:40 +0000

    - added ByteBitArray

---
+++ sources/AnjLab.FX/Collections/ByteBitArray.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using AnjLab.FX.System;
+
+namespace AnjLab.FX.Collections
+{
+    public class ByteBitArray
+    {
+        private readonly BitArray _bitArrray;
+        private readonly int _arrayBase = 8;
+
+        public ByteBitArray() : this(0)
+        {
+        }
+
+        public ByteBitArray(byte value)
+        {
+            _bitArrray = new BitArray(new bool[_arrayBase]);
+            for (int i = 0; i < _arrayBase; i++)
+            {
+                byte mask = 1;
+                mask = (byte)(mask << i);
+                _bitArrray[i] = (value & mask) > 0;
+            }
+        }
+
+        public byte GetByte()
+        {
+            byte result = 0;
+            for (int i = 0; i < _arrayBase; i++)
+            {
+                byte mask = (byte)((_bitArrray[i]) ? 1 : 0);
+                mask = (byte)(mask << i);
+                result = (byte)(result | mask);
+            }
+            return result;
+        }
+
+        public bool this[int index]
+        {
+            get { return _bitArrray[index]; }
+            set { _bitArrray[index] = value; }
+        }
+    }
+}
