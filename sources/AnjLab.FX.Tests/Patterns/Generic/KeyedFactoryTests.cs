bc28971680ff17b1eff755115c16c91121880fa1
Yury Korolev <yury.korolev@gmail.com>
2007-11-22 22:31:18 +0000

    added KeyedFactoryTests

--- sources/AnjLab.FX.Tests/Patterns/Generic/KeyedFactoryTests.cs
+++ sources/AnjLab.FX.Tests/Patterns/Generic/KeyedFactoryTests.cs
@@ -1,8 +1,19 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
 using AnjLab.FX.Patterns.Generic;
 using NUnit.Framework;
 
 namespace AnjLab.FX.Tests.Patterns.Generic
 {
+    public class Product: ICloneable
+    {
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+
     [TestFixture]
     public class KeyedFactoryTests
     {
@@ -15,6 +26,35 @@ namespace AnjLab.FX.Tests.Patterns.Generic
 
             Assert.AreEqual("bla", f.Create("bla"));
             Assert.AreEqual("bla1", f.Create("bla1"));
+        }
+
+        [Test, Ignore]
+        public void TestFactorySpeed()
+        {
+            KeyedFactory<int, Product> factory = new KeyedFactory<int, Product>();
+            int key = 0;
+            factory.RegisterType<Product>(key++);
+            factory.RegisterMethod(key++, delegate { return Activator.CreateInstance<Product>(); });
+            factory.RegisterMethod(key++, delegate { return (Product) Activator.CreateInstance(typeof (Product));}) ;
+            factory.RegisterMethod(key++, delegate { return new Product(); });
+            factory.RegisterImmutable(key++, new Product());
+            factory.RegisterPrototype(key++, new Product());
+
+            ConstructorInfo ci = typeof (Product).GetConstructor(Type.EmptyTypes);
+            factory.RegisterMethod(key++, delegate { return (Product) ci.Invoke(null); });
+
+            foreach (int k in factory.Keys)
+                factory.Create(k);
+
+            int iterationCount = 1000000;
+
+            foreach (int k in factory.Keys)
+            {
+                Stopwatch watch = Stopwatch.StartNew();
+                for (int i = 0; i < iterationCount; i++)
+                    factory.Create(k);
+                Console.WriteLine("{0} {1}", k, watch.ElapsedMilliseconds);
+            }
         }
     }
 }
