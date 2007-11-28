7049f89cf2b6b648c4067ef2b9af42a816c8f022
Alex Ilin <alex.a.ilin@gmail.com>
2007-11-28 09:04:21 +0000

    - added one more constructor

--- sources/AnjLab.FX/IO/Ports/SerialPortDelay.cs
+++ sources/AnjLab.FX/IO/Ports/SerialPortDelay.cs
@@ -8,24 +8,31 @@ namespace AnjLab.FX.IO.Ports
     public class SerialPortDelay
     {
         private SerialPort _port;
-        TimeSpan _PreWrite = TimeSpan.FromMilliseconds(0);
-        TimeSpan _PostWrite = TimeSpan.FromMilliseconds(0);
+        TimeSpan _preWrite = TimeSpan.FromMilliseconds(0);
+        TimeSpan _postWrite = TimeSpan.FromMilliseconds(0);
 
         public SerialPortDelay(SerialPort serialPort)
         {
             _port = serialPort;
         }
 
+        public SerialPortDelay(SerialPort port, TimeSpan preWrite, TimeSpan postWrite)
+        {
+            _port = port;
+            _preWrite = preWrite;
+            _postWrite = postWrite;
+        }
+
         public void Write(byte[] data)
         {
             Guard.IsTrue(_port.IsOpen);
 
-            Thread.Sleep(_PreWrite);
+            Thread.Sleep(_preWrite);
             _port.RtsEnable = true;
 
             _port.Write(data, 0, data.Length);
 
-            Thread.Sleep(_PostWrite);
+            Thread.Sleep(_postWrite);
             _port.RtsEnable = false;
         }
 
@@ -37,14 +44,14 @@ namespace AnjLab.FX.IO.Ports
 
         public TimeSpan PreWrite
         {
-            get { return _PreWrite; }
-            set { _PreWrite = value; }
+            get { return _preWrite; }
+            set { _preWrite = value; }
         }
 
         public TimeSpan PostWrite
         {
-            get { return _PostWrite; }
-            set { _PostWrite = value; }
+            get { return _postWrite; }
+            set { _postWrite = value; }
         }
     }
 }
