05cf8cf23912170320887abc09b346d3221bf625
Alex Ilin <alex.a.ilin@gmail.com>
2007-12-25 20:59:23 +0000

    - added some statistics classes and directory helper

---
+++ sources/AnjLab.FX/IO/DirectoryHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using AnjLab.FX.System;
+
+namespace AnjLab.FX.IO
+{
+    public class DirectoryHelper
+    {
+        public static void CopyDirectory(string sourceDir, string copyPath)
+        {
+            Guard.IsTrue(Directory.Exists(sourceDir));
+
+            if (!Directory.Exists(copyPath))
+                Directory.CreateDirectory(copyPath);
+
+            foreach (string file in Directory.GetFiles(sourceDir))
+                File.Copy(file, Path.Combine(copyPath, Path.GetFileName(file)));
+
+            foreach (string dir in Directory.GetDirectories(sourceDir))
+                CopyDirectory(dir, Path.Combine(copyPath, Path.GetDirectoryName(dir)));
+        }
+    }
+}
---
+++ sources/AnjLab.FX/Mathematics/DiscreteVariate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnjLab.FX.Mathematics
+{
+    public class DiscreteVariate
+    {
+        private readonly IList<double> _values = new List<double>();
+
+        public DiscreteVariate()
+        {
+        }
+
+        public DiscreteVariate(IEnumerable<double> values)
+        {
+            foreach (double value in values)
+                _values.Add(value);
+        }
+
+        public void AddValue(double value)
+        {
+            _values.Add(value);
+        }
+
+        public double Mx
+        {
+            get
+            {
+                double sum = 0;
+                foreach (double value in _values)
+                    sum += value;
+                return sum / _values.Count;
+            }
+        }
+
+        public double Dx
+        {
+            get
+            {
+                double mathExpectation = Mx;
+
+                DiscreteVariate var = new DiscreteVariate();
+                foreach (double value in _values)
+                    var.AddValue(Math.Pow(value - mathExpectation, 2));
+
+                return var.Mx;
+            }
+        }
+
+        public double Sx
+        {
+            get { return Math.Sqrt(Dx); }
+        }
+
+        public double MathExpectation { get { return Mx; } }
+        public double Dispersion { get { return Dx; } }
+        public double QuadraticDeviation { get { return Sx; } }
+    }
+}
