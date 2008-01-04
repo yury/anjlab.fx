98dd6fafee1c08fbeb4ec3c2e209e833fa35be42
Alex Ilin <alex.a.ilin@gmail.com>
2008-01-04 14:54:01 +0000

    - wpf helpers

---
+++ sources/AnjLab.FX.Wpf/VisualTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace AnjLab.FX.Wpf
+{
+    public class VisualTree
+    {
+        public static DependencyObject GetDependencyObject(DependencyObject startObject, Type type)
+        {
+            //Walk the visual tree to get the parent(ItemsControl) 
+            //of this control
+            DependencyObject parent = startObject;
+            while (parent != null)
+            {
+                if (type.IsInstanceOfType(parent))
+                    break;
+                else
+                    parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent;
+        }
+    }
+}
