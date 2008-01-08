8ba2976922c063ab9bbc12e25302046135e8216f
Alex Ilin <alex.a.ilin@gmail.com>
2008-01-08 16:14:45 +0000

    - added Operation.Execute methods

---
+++ sources/AnjLab.FX/System/Operation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AnjLab.FX.System
+{
+    public class Operation
+    {
+        public static void Execute<TIgnoreException>(VoidAction operation)
+            where TIgnoreException : Exception
+        {
+            try
+            {
+                operation();
+            }
+            catch (TIgnoreException)
+            {
+            }
+        }
+
+        public static TProduct Execute<TProduct, TIgnoreException>(FactoryMethod<TProduct> operation)
+            where TIgnoreException : Exception
+           where TProduct : class
+        {
+            try
+            {
+                return operation();
+            }
+            catch (TIgnoreException)
+            {
+            }
+            return null;
+        }
+
+        public static TProduct Execute<TProduct, TSource, TIgnoreException>(FactoryMethod<TProduct, TSource> operation, TSource source)
+            where TIgnoreException : Exception
+            where TProduct : class
+        {
+            try
+            {
+                return operation(source);
+            }
+            catch (TIgnoreException)
+            {
+            }
+            return null;
+        }
+    }
+}
