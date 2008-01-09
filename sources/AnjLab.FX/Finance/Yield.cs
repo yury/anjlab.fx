5f70a59965a1e21d6c8c887959a0d8fc9d80a627
Alex Ilin <alex.a.ilin@gmail.com>
2008-01-09 14:52:48 +0000

    - add new operations

--- sources/AnjLab.FX/Finance/Yield.cs
+++ sources/AnjLab.FX/Finance/Yield.cs
@@ -14,5 +14,24 @@ namespace AnjLab.FX.Finance
             decimal time = TimeInterval.Get(currentDate, futureDate);
             return (time != 0) ? ((100*(100 - pricePercent))/(time*pricePercent)) : 0;
         }
+
+        public static decimal CalculatePrice(decimal yield, DateTime currentDate, DateTime futureDate)
+        {
+            Guard.ArgumentNotNull("currentDate", currentDate);
+            Guard.ArgumentNotNull("futureDate", futureDate);
+
+            decimal time = TimeInterval.Get(currentDate, futureDate);
+            return (time != 0) ? (100/(yield*time + 100))*100 : 0;
+        }
+
+        public static decimal CalculatePrice(decimal nominal, decimal amount)
+        {
+            return 100 * amount / nominal;
+        }
+
+        public static decimal CalculateAmount(decimal nominal, decimal price)
+        {
+            return nominal * price / 100;
+        }
     }
 }
