47a22250a5ff119b99018f50e64be091f894b892
Yury Korolev <yury.korolev@gmail.com>
2008-01-27 15:48:21 +0000

    Added PriorityQueue

---
+++ sources/AnjLab.FX.Tests/Collections/PriorityQueueTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnjLab.FX.Collections;
+using NUnit.Framework;
+
+namespace AnjLab.FX.Tests.Collections
+{
+    [TestFixture]
+    public class PriorityQueueTests
+    {
+        [Test]
+        public void TestCount()
+        {
+            PriorityQueue<int, string> q = new PriorityQueue<int, string>();
+            q.Enqueue(0, "a");
+            q.Enqueue(-1, "b");
+            q.Enqueue(1, "c");
+
+            Assert.AreEqual(3, q.Count);
+        }
+
+        [Test]
+        public void TestOrder()
+        {
+            PriorityQueue<int, string> q = new PriorityQueue<int, string>();
+            q.Enqueue(0, "a");
+            q.Enqueue(-1, "b");
+            q.Enqueue(1, "c");
+
+            Assert.AreEqual("b", q.Dequeue());
+            Assert.AreEqual("a", q.Dequeue());
+            Assert.AreEqual("c", q.Dequeue());
+
+            q.Enqueue(0, "a");
+            q.Enqueue(0, "b");
+            q.Enqueue(0, "c");
+
+            q.Enqueue(1, "c");
+            q.Enqueue(1, "d");
+            q.Enqueue(1, "e");
+
+            Assert.AreEqual("a", q.Dequeue());
+            Assert.AreEqual("b", q.Dequeue());
+            Assert.AreEqual("c", q.Dequeue());
+
+            Assert.AreEqual("c", q.Dequeue());
+            Assert.AreEqual("d", q.Dequeue());
+            Assert.AreEqual("e", q.Dequeue());
+        }
+
+        [Test]
+        public void TestOrderWithRandom()
+        {
+            PriorityQueue<int, int> q = new PriorityQueue<int, int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                int v = rnd.Next(-1000, 1000);
+                q.Enqueue(v, v);
+            }
+            Assert.AreEqual(1000, q.Count);
+            int prevValue = int.MinValue;
+            foreach (int value in q)
+            {
+                Assert.IsTrue(prevValue <= value);
+            }
+        }
+
+        [Test]
+        public void TestPriorityDequeue()
+        {
+            PriorityQueue<int, int> q = new PriorityQueue<int, int>();
+            q.Enqueue(1, 1);
+            q.Enqueue(1, 2);
+            q.Enqueue(1, 3);
+            q.Enqueue(1, 4);
+
+            q.Enqueue(10, 11);
+            q.Enqueue(10, 12);
+            q.Enqueue(10, 13);
+            q.Enqueue(10, 14);
+
+            Assert.AreEqual(8, q.Count);
+
+            Assert.AreEqual(4, q.GetPriorityCount(1));
+            Assert.AreEqual(1, q.Dequeue(1));
+            Assert.AreEqual(2, q.Dequeue(1));
+            Assert.AreEqual(3, q.Dequeue(1));
+            Assert.AreEqual(4, q.Dequeue(1));
+            Assert.AreEqual(0, q.GetPriorityCount(1));
+
+            Assert.AreEqual(4, q.Count);
+
+            Assert.AreEqual(4, q.GetPriorityCount(10));
+            Assert.AreEqual(11, q.Dequeue(10));
+            Assert.AreEqual(12, q.Dequeue(10));
+            Assert.AreEqual(13, q.Dequeue(10));
+            Assert.AreEqual(14, q.Dequeue(10));
+            Assert.AreEqual(0, q.GetPriorityCount(10));
+        }
+
+        [Test]
+        public void TestPeek()
+        {
+            PriorityQueue<int, int> q = new PriorityQueue<int, int>();
+            q.Enqueue(2, 2);
+            q.Enqueue(1, 1);
+            Assert.AreEqual(1, q.Peek());
+            Assert.AreEqual(2, q.Count);
+        }
+    }
+}
---
+++ sources/AnjLab.FX/Collections/PriorityQueue.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AnjLab.FX.Collections
+{
+    public class PriorityQueue<TPriority, TItem>: ICollection, IEnumerable<TItem>
+    {
+        readonly SortedList<TPriority, Queue<TItem>> _parts = new SortedList<TPriority, Queue<TItem>>();
+        private int _count;
+        private readonly bool _isReadOnly = false;
+        private readonly object _syncRoot = new object();
+        private readonly bool _isSynchronized = false;
+
+        public void CopyTo(Array array, int index)
+        {
+            ArrayList items = new ArrayList(Count);
+            foreach (TItem item in this)
+            {
+                items.Add(item);
+            }
+            items.CopyTo(array, index);
+        }
+
+        public object SyncRoot
+        {
+            get { return _syncRoot; }
+        }
+
+        public bool IsSynchronized
+        {
+            get { return _isSynchronized; }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public int GetPriorityCount(TPriority priority)
+        {
+            if (!_parts.ContainsKey(priority))
+                return 0;
+            return _parts[priority].Count;
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _isReadOnly; }
+        }
+
+        public void Enqueue(TPriority priority, TItem item)
+        {
+            if (!_parts.ContainsKey(priority))
+            {
+                _parts.Add(priority, new Queue<TItem>());
+            }
+            _parts[priority].Enqueue(item);
+            _count++;
+        }
+
+        public TItem Dequeue()
+        {
+            foreach (KeyValuePair<TPriority, Queue<TItem>> part in _parts)
+            {
+                if (part.Value.Count == 0)
+                    continue;
+                _count--;
+                return part.Value.Dequeue();
+            }
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        public TItem Dequeue(TPriority priority)
+        {
+            if (!_parts.ContainsKey(priority))
+                throw new InvalidOperationException("Queue is empty");
+            Queue<TItem> items = _parts[priority];
+            if (items.Count > 0 )
+            {
+                _count--;
+                return items.Dequeue();
+            }
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        public TItem Peek()
+        {
+            foreach (KeyValuePair<TPriority, Queue<TItem>> part in _parts)
+            {
+                if (part.Value.Count == 0)
+                    continue;
+                
+                return part.Value.Peek();
+            }
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        IEnumerator<TItem> IEnumerable<TItem>.GetEnumerator()
+        {
+            return GetTypedEnumerator();
+        }
+
+        IEnumerator<TItem> GetTypedEnumerator()
+        {
+            foreach (KeyValuePair<TPriority, Queue<TItem>> part in _parts)
+            {
+                foreach (TItem item in part.Value)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetTypedEnumerator();
+        }
+    }
+}
