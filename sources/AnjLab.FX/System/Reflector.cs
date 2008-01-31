cd139b24b8ac9f2b50f36b65a2596cbce954759b
Alex Ilin <alex.a.ilin@gmail.com>
2008-01-31 16:39:53 +0000

    - added reflector

---
+++ sources/AnjLab.FX/System/Reflector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace AnjLab.FX.System
+{
+    public class Reflector
+    {
+        public static object GetValue(object obj, string valuePath)
+        {
+            return GetValue(obj, valuePath, BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        public static object GetValue(object obj, string valuePath, BindingFlags flags)
+        {
+            try
+            {
+                string[] path = valuePath.Split('.');
+
+                foreach (string pathPart in path)
+                    obj = obj.GetType().GetProperty(pathPart, flags).GetValue(obj, null);
+
+                return obj;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
