c3717c9b1e4484289e82e63636aad9f47ad55c42
Yury Korolev <yury.korolev@gmail.com>
2008-02-08 08:35:12 +0000

    fixed bug in YiledTest

--- sources/AnjLab.FX.Tests/Finance/YieldTests.cs
+++ sources/AnjLab.FX.Tests/Finance/YieldTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using NUnit.Framework;
 
 namespace AnjLab.FX.Finance
@@ -9,12 +10,13 @@ namespace AnjLab.FX.Finance
         [Test]
         public void CalculateTest()
         {
-            Assert.AreEqual(11.392, Math.Round(Yield.Calculate(DateTime.Parse("01.10.2006"), DateTime.Parse("22.9.2007"), 90), 3));
-            Assert.AreEqual(52.1368, Math.Round(Yield.Calculate(DateTime.Parse("05.09.2008"), DateTime.Parse("22.11.2008"), 90), 4));
-            Assert.AreEqual(2.009, Math.Round(Yield.Calculate(DateTime.Parse("03.10.2003"), DateTime.Parse("30.10.2005"), 96), 4));
-            Assert.AreEqual(1.1042, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999"), DateTime.Parse("25.10.2009"), 90), 4));
+            CultureInfo ci = CultureInfo.GetCultureInfo("Ru-ru");
+            Assert.AreEqual(11.392, Math.Round(Yield.Calculate(DateTime.Parse("01.10.2006", ci), DateTime.Parse("22.9.2007", ci), 90), 3));
+            Assert.AreEqual(52.1368, Math.Round(Yield.Calculate(DateTime.Parse("05.09.2008", ci), DateTime.Parse("22.11.2008", ci), 90), 4));
+            Assert.AreEqual(2.009, Math.Round(Yield.Calculate(DateTime.Parse("03.10.2003", ci), DateTime.Parse("30.10.2005", ci), 96), 4));
+            Assert.AreEqual(1.1042, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999", ci), DateTime.Parse("25.10.2009", ci), 90), 4));
 
-            Assert.AreEqual(0, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999"), DateTime.Parse("02.10.1999"), 90), 4));
+            Assert.AreEqual(0, Math.Round(Yield.Calculate(DateTime.Parse("02.10.1999", ci), DateTime.Parse("02.10.1999", ci), 90), 4));
         }
     }
 }
