4a73f48375d92bfd10237d3529adaf0c6611df26
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-02-11 15:40:22 +0000

    added filter

---
+++ sources/AnjLab.FX/Data/RepositoryFilterType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnjLab.FX.Data
+{
+    public enum RepositoryFilterType
+    {
+        ByMinute  = 0,
+        ByHour    = 1,
+        ByDay     = 2,
+        ByWeek    = 3,
+        ByMonth   = 4,
+        ByQuarter = 5,
+        ByYear    = 6
+    }
+}
