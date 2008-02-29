6a8cfe8ed0253cc0cd4741757376889b5409011c
Yury Korolev <yury.korolev@gmail.com>
2008-02-29 14:30:01 +0000

    useful wrapper for wcf services

---
+++ sources/AnjLab.FX/System/ServiceChannel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace AnjLab.FX.System
+{
+    public class ServiceChannel<TServiceContract>: IDisposable
+    {
+        private readonly TServiceContract _service;
+
+        public ServiceChannel(TServiceContract service)
+        {
+            Guard.ArgumentNotNull("service", service);
+            _service = service;
+        }
+
+        public TServiceContract Service
+        {
+            get { return _service; }
+        }
+
+        public void Dispose()
+        {
+            if (_service == null)
+                return;
+            var channel = _service as IChannel;
+            if (channel != null)
+                channel.Close();
+        }
+    }
+}
