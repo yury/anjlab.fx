39626545e79364196209071e278c644660bf66d2
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-03-07 16:00:46 +0000

    fix case format provider test

--- sources/AnjLab.FX.Tests/System/CaseFormatProviderTests.cs
+++ sources/AnjLab.FX.Tests/System/CaseFormatProviderTests.cs
@@ -20,8 +20,8 @@ namespace AnjLab.FX.Tests.System
             Assert.AreEqual("Zero", string.Format(provider, "{0:0=Zero;1=One;2=Two;we}", 0));
             Assert.AreEqual("Two", string.Format(provider, "{0:0=Zero;1=One;2=Two}", 2));
             Assert.AreEqual("2", string.Format(provider, "{0:0=Zero;1=One;23=Two}", 2));
-            Assert.AreEqual("2,22", string.Format(provider, "{0:F2}", 2.2222));
-            Assert.AreEqual("2,23", string.Format(provider, "{0:0.00}", 2.2299));
+            Assert.AreEqual(2.2222.ToString("F2"), string.Format(provider, "{0:F2}", 2.2222));
+            Assert.AreEqual(2.2299.ToString("0.00"), string.Format(provider, "{0:0.00}", 2.2299));
             Assert.AreEqual("2=Two", string.Format("{0:2=Two}", 2));
             Assert.AreEqual("57%", string.Format(provider, "{0:p0}", 0.57));
             Assert.AreEqual(string.Empty, string.Format("{0:2=Two}", new object[] {null}));
