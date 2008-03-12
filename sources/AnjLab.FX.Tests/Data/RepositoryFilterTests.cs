a67a688850d32e6712ec88e64ce65eb7305cee37
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-03-12 15:13:03 +0000

    added repository filter class

---
+++ sources/AnjLab.FX.Tests/Data/RepositoryFilterTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AnjLab.FX.Data;
+using System.Globalization;
+
+namespace AnjLab.FX.Tests.Data
+{
+    [TestFixture]
+    public class RepositoryFilterTests
+    {
+        [Test]
+        public void TestIncrementDate()
+        {
+            Assert.AreEqual(new DateTime(2008, 01, 02), RepositoryFilter.IncrementDateByRepositoryFilter(new DateTime(2008, 01, 01), RepositoryFilterType.ByDay));
+            Assert.AreEqual(new DateTime(2008, 01, 01, 02, 00, 00), RepositoryFilter.IncrementDateByRepositoryFilter(new DateTime(2008, 01, 01, 01, 00, 00), RepositoryFilterType.ByHour));
+            Assert.AreEqual(new DateTime(2008, 01, 01, 01, 01, 00), RepositoryFilter.IncrementDateByRepositoryFilter(new DateTime(2008, 01, 01, 01, 00, 00), RepositoryFilterType.ByMinute));
+            Assert.AreEqual(new DateTime(2008, 02, 01), RepositoryFilter.IncrementDateByRepositoryFilter(new DateTime(2008, 01, 01), RepositoryFilterType.ByMonth));
+            Assert.AreEqual(new DateTime(2008, 01, 08), RepositoryFilter.IncrementDateByRepositoryFilter(new DateTime(2008, 01, 01), RepositoryFilterType.ByWeek));
+            Assert.AreEqual(new DateTime(2009, 01, 01), RepositoryFilter.IncrementDateByRepositoryFilter(new DateTime(2008, 01, 01), RepositoryFilterType.ByYear));
+        }
+
+        [Test]
+        public void TestTrimDate()
+        {
+            DateTime t = new DateTime(2008, 02, 01, 15, 33, 14);
+            Assert.AreEqual(new DateTime(2008, 02, 01), RepositoryFilter.TrimDateTime(t, RepositoryFilterType.ByDay));
+            Assert.AreEqual(new DateTime(2008, 02, 01, 15, 00, 00), RepositoryFilter.TrimDateTime(t, RepositoryFilterType.ByHour));
+            Assert.AreEqual(new DateTime(2008, 02, 01, 15, 33, 00), RepositoryFilter.TrimDateTime(t, RepositoryFilterType.ByMinute));
+            Assert.AreEqual(new DateTime(2008, 02, 01, 00, 00, 00), RepositoryFilter.TrimDateTime(t, RepositoryFilterType.ByMonth));
+            Assert.AreEqual(new DateTime(2008, 01, 27, 00, 00, 00), RepositoryFilter.TrimDateTime(t, RepositoryFilterType.ByWeek));
+            Assert.AreEqual(new DateTime(2008, 01, 01, 00, 00, 00), RepositoryFilter.TrimDateTime(t, RepositoryFilterType.ByYear));
+        }
+    }
+}
---
+++ sources/AnjLab.FX/Data/RepositoryFilter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace AnjLab.FX.Data
+{
+    public class RepositoryFilter
+    {
+        public static DateTime TrimDateTime(DateTime dateTime, RepositoryFilterType filterType)
+        {
+            switch(filterType)
+            {
+                case RepositoryFilterType.ByWeek:
+                    return dateTime.Date.AddDays(-(int) dateTime.DayOfWeek);
+                default:
+                    string format = GetDateFormatByRepositoryFilter(filterType);
+                    return DateTime.ParseExact(dateTime.ToString(format), format, CultureInfo.InvariantCulture);
+            }
+        }
+
+        public static string GetDateFormatByRepositoryFilter(RepositoryFilterType filterType)
+        {
+            switch (filterType)
+            {
+                case RepositoryFilterType.None:
+                    return "yyyy.MM.dd H:mm:ss";
+                case RepositoryFilterType.ByMinute:
+                    return "yyyy.MM.dd H:mm";
+                case RepositoryFilterType.ByHour:
+                    return "yyyy.MM.dd H:00";
+                case RepositoryFilterType.ByWeek:
+                case RepositoryFilterType.ByDay:
+                    return "yyyy.MM.dd";
+                case RepositoryFilterType.ByMonth:
+                    return "yyyy.MM.01";
+                case RepositoryFilterType.ByYear:
+                    return "yyyy.01.01";
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static DateTime IncrementDateByRepositoryFilter(DateTime dateTime, RepositoryFilterType filterType)
+        {
+            switch (filterType)
+            {
+                case RepositoryFilterType.None:
+                    return dateTime;
+                case RepositoryFilterType.ByMinute:
+                    return dateTime.AddMinutes(1);
+                case RepositoryFilterType.ByHour:
+                    return dateTime.AddHours(1);
+                case RepositoryFilterType.ByDay:
+                    return dateTime.AddDays(1);
+                case RepositoryFilterType.ByMonth:
+                    return dateTime.AddMonths(1);
+                case RepositoryFilterType.ByYear:
+                    return dateTime.AddYears(1);
+                case RepositoryFilterType.ByWeek:
+                    return dateTime.AddDays(7);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
