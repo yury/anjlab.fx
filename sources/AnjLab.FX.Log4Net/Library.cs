b5931991787df20434fa7a80c23552292f6db4ce
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-03-18 16:26:42 +0000

    added log4net project

---
+++ sources/AnjLab.FX.Log4Net/Library.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnjLab.FX.Log4Net
+{
+    class Library
+    {
+    }
+}
---
+++ sources/AnjLab.FX.Log4Net/Log4NetLog.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnjLab.FX.IO;
+using System.IO;
+
+namespace AnjLab.FX.Log4Net
+{
+    class Log4NetLog : ILog
+    {
+        private readonly log4net.ILog _log;
+        private static readonly string _relativePathToConfig = @"Properties\log4net.config";
+
+        public static void LoadLog4NetConfig()
+        {
+            LoadLog4NetConfig(_relativePathToConfig);
+        }
+
+        public static void LoadLog4NetConfig(string relativePathToConfig)
+        {
+            string pathToConfig = Path.Combine(Path.GetDirectoryName(typeof(Log4NetLog).Assembly.Location), relativePathToConfig);
+            if (!File.Exists(pathToConfig))
+            {
+
+                pathToConfig = Path.Combine(Environment.CurrentDirectory, relativePathToConfig);
+            }
+
+            FileInfo fi = new FileInfo(pathToConfig);
+            if (fi.Exists)
+            {
+                log4net.Config.XmlConfigurator.ConfigureAndWatch(fi);
+            } 
+            else
+            {
+                log4net.Config.BasicConfigurator.Configure();
+            }
+        }
+
+        public Log4NetLog(string name)
+        {
+            _log = log4net.LogManager.GetLogger(name);
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            if (_log.IsInfoEnabled)
+                _log.InfoFormat(message, args);
+        }
+
+        public void Debug(string message, params object[] args)
+        {
+            if (_log.IsDebugEnabled)
+                _log.DebugFormat(message, args);
+        }
+
+        public void Warning(string message, params object[] args)
+        {
+            if (_log.IsWarnEnabled)
+                _log.WarnFormat(message, args);
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            if (_log.IsErrorEnabled)
+                _log.ErrorFormat(message, args);
+        }
+
+        public void Fatal(string message, params object[] args)
+        {
+            if (_log.IsFatalEnabled)
+                _log.FatalFormat(message, args);
+        }
+
+        public void Info(string message)
+        {
+            if (_log.IsInfoEnabled)
+                _log.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            if (_log.IsWarnEnabled)
+                _log.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            if (_log.IsErrorEnabled)
+                _log.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            if (_log.IsFatalEnabled)
+                _log.Fatal(message);
+        }
+
+        public void Debug(string message)
+        {
+            if (_log.IsDebugEnabled)
+                _log.Debug(message);
+        }
+
+        public bool IsInfoEnabled
+        {
+            get { return _log.IsInfoEnabled; }
+        }
+
+        public bool IsWarnEnabled
+        {
+            get { return _log.IsWarnEnabled; }
+        }
+
+        public bool IsErrorEnabled
+        {
+            get { return _log.IsErrorEnabled; }
+        }
+
+        public bool IsFatalEnabled
+        {
+            get { return _log.IsFatalEnabled; }
+        }
+
+        public bool IsDebugEnabled
+        {
+            get { return _log.IsDebugEnabled; }
+        }
+    }
+}
