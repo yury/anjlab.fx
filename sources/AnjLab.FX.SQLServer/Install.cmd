e1e379fadc4e368be361d5a2182ae16792c53fb1
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-03-26 12:51:06 +0000

    change install script

--- sources/AnjLab.FX.SQLServer/Install.cmd
+++ sources/AnjLab.FX.SQLServer/Install.cmd
@@ -1,14 +1,12 @@
 @echo off
 
-set server=.
-set database=123
-set user=sa
-set password=Friday13
+set server=%1%
+set database=%2%
 
 rem svn up
 
-osql -S %server% -U %user% -P %password% -d %database% -i schema.sql
-for /f "tokens=*" %%a in ('dir /b tables\*.sql') do osql -S %server% -U %user% -P %password% -d %database% -i tables\%%a  
-for /f "tokens=*" %%a in ('dir /b functions\*.sql') do osql -S %server% -U %user% -P %password% -d %database% -i functions\%%a 
-for /f "tokens=*" %%a in ('dir /b procedures\*.sql') do osql -S %server% -U %user% -P %password% -d %database% -i procedures\%%a 
+osql -S %server% -E -d %database% -i schema.sql
+for /f "tokens=*" %%a in ('dir /b tables\*.sql') do osql -S %server% -E -d %database% -i tables\%%a  
+for /f "tokens=*" %%a in ('dir /b functions\*.sql') do osql -S %server% -E -d %database% -i functions\%%a 
+for /f "tokens=*" %%a in ('dir /b procedures\*.sql') do osql -S %server% -E -d %database% -i procedures\%%a 
 
