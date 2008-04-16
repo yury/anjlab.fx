35f3870d3f46c773771b091db316a56df37b827d
Alex Ilin <alex.a.ilin@gmail.com>
2008-04-16 09:08:27 +0000

    - piar tests

---
+++ sources/AnjLab.FX.Tests/System/PairTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using AnjLab.FX.System;
+using NUnit.Framework;
+
+namespace AnjLab.FX.Tests.System
+{
+    [TestFixture]
+    public class PairTests
+    {
+        [Test]
+        public void TestEquals()
+        {
+            Pair<object, object> tom = new Pair<object, object>(null, null);
+            Pair<object, object> bill = new Pair<object, object>(null, null);
+            
+            Assert.AreEqual(tom, bill);
+            
+            tom.A = 2;
+            Assert.AreNotEqual(tom, bill);
+
+            bill.A = 2;
+            Assert.AreEqual(tom, bill);
+
+            tom.B = 2;
+            Assert.AreNotEqual(tom, bill);
+
+            bill.B = 2;
+            Assert.AreEqual(tom, bill);
+        }
+    }
+}
