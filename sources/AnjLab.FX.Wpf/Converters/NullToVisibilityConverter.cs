f9a50bdd31f06993dd89b92a74b29083138c4f73
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-04-22 17:07:34 +0000

    add converters

---
+++ sources/AnjLab.FX.Wpf/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null || (parameter != null && value.ToString() == parameter.ToString()) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
