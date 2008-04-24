b9c0c047e2c064c9641d39bb174a7a01da2064ce
Yury Korolev <yury.korolev@gmail.com>
2008-04-24 12:41:43 +0000

    added Sys/StringExtentions :  - StartProcess  - IsNullOrEmpty

---
+++ sources/AnjLab.FX/Sys/StringExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AnjLab.FX.Sys
+{
+    public static class StringExtentions
+    {
+        public static void StartProcess(this string cmd)
+        {
+            var p = new Process {StartInfo = new ProcessStartInfo(cmd)};
+            p.Start();
+        }
+
+        public static bool IsNullOrEmtpy(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+    }
+}
