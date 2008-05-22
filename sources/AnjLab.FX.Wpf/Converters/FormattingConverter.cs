6a5b3cfc4433a213c9869d820cddc10409c3a473
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-05-22 12:19:12 +0000

    add formatting converter

---
+++ sources/AnjLab.FX.Wpf/Converters/FormattingConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class FormattingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var formatString = parameter as string;
+            return formatString != null ? string.Format(culture, formatString, value) : value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // we don't intend this to ever be called
+            return null;
+        }
+    }
+}
