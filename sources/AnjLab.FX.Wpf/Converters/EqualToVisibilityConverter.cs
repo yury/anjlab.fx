ed8a28153968db731fbacec31cb508f813f2235f
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-05-23 13:19:33 +0000

    add equal to visibility converter

---
+++ sources/AnjLab.FX.Wpf/Converters/EqualToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    public class EqualToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !Equals(value, parameter) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
