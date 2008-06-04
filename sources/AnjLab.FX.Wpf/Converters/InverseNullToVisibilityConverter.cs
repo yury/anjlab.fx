3c1e0c498c58e873a68f1d664a695a145b10527e
Sergey Balashov <sergey.l.balashov@gmail.com>
2008-06-04 19:05:12 +0000

    add InverseNullToVisibilityConverter

---
+++ sources/AnjLab.FX.Wpf/Converters/InverseNullToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    public class InverseNullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null || (parameter != null && value.ToString() == parameter.ToString()) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
