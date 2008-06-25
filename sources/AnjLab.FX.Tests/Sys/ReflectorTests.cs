83eb84e219d6cb4538f758bea439042ede718e08
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-06-25 17:04:42 +0000

    add reflector method

---
+++ sources/AnjLab.FX.Tests/Sys/ReflectorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AnjLab.FX.Sys;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace AnjLab.FX.Tests.Sys
+{
+    [TestFixture]
+    public class ReflectorTests
+    {
+        public class TestClass1
+        { 
+        }
+
+        public class TestClass2
+        {
+            public string TestField1;
+        }
+
+        public class TestClass3
+        {
+            [System.ComponentModel.Description("Тестовое поле")]
+            public string TestField1 = "Test";
+
+            [Browsable(true)]
+            public string TestProperty1 { get; set; }
+
+            [Browsable(false)]
+            public string TestProperty2 { get; set; }
+        }
+
+        [Test]
+        public void TestGetMemberEnumerable()
+        {
+            var result = Reflector.GetMemberEnumerable(new TestClass1());
+            Assert.AreEqual(0, result.Count());
+
+            result = Reflector.GetMemberEnumerable(new TestClass2());
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual("TestField1", result.First().Key);
+            Assert.AreEqual(null, result.First().Value);
+
+            result = Reflector.GetMemberEnumerable(new TestClass3 { TestProperty1 = "Test"});
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("Тестовое поле", result.First().Key);
+            Assert.AreEqual("Test", result.First().Value);
+
+            Assert.AreEqual("TestProperty1", result.ToArray()[1].Key);
+            Assert.AreEqual("Test", result.ToArray()[1].Value);
+        }
+    }
+}
--- sources/AnjLab.FX/Sys/Reflector.cs
+++ sources/AnjLab.FX/Sys/Reflector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
-
+using System.ComponentModel;
 namespace AnjLab.FX.Sys
 {
     public class Reflector
@@ -28,6 +29,44 @@ namespace AnjLab.FX.Sys
             {
                 return null;
             }
+        }
+
+        public static IEnumerable<KeyValuePair<string, object>> GetMemberEnumerable(object obj)
+        {
+            return GetMemberEnumerable(obj, BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        public static IEnumerable<KeyValuePair<string, object>> GetMemberEnumerable(object obj, BindingFlags flags)
+        {
+            foreach(var field in obj.GetType().GetFields(flags))
+            {
+                if(IsBrowsable(field))
+                    yield return new KeyValuePair<string, object>(GetMemberDescription(field), field.GetValue(obj));
+            }
+
+            foreach(var property in obj.GetType().GetProperties(flags))
+            {
+                if(IsBrowsable(property))
+                    yield return new KeyValuePair<string, object>(GetMemberDescription(property), property.GetValue(obj, null));
+            }
+        }
+
+        private static bool IsBrowsable(MemberInfo info)
+        {
+            var attrs = info.GetCustomAttributes(typeof(BrowsableAttribute), false);
+
+            if (attrs == null || attrs.Length == 0) return true;
+
+            return ((BrowsableAttribute)attrs[0]).Browsable; 
+        }
+
+        private static string GetMemberDescription(MemberInfo info)
+        {
+            var attrs = info.GetCustomAttributes(typeof (DescriptionAttribute), false);
+
+            if (attrs == null || attrs.Length == 0) return info.Name;
+
+            return ((DescriptionAttribute) attrs[0]).Description;
         }
     }
 }
