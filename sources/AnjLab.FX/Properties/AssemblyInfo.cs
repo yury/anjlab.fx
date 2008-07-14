72e9e529b515e767eba891e1e4f2563af6d7fd72
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-07-14 15:52:04 +0000

    sign anjlab.fx

--- sources/AnjLab.FX/Properties/AssemblyInfo.cs
+++ sources/AnjLab.FX/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -11,10 +13,21 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("AnjLab.FX.Tests")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: InternalsVisibleTo("AnjLab.FX.Tests, PublicKey="
+  +  "00240000048000009400000006020000002400005253413100040000010001005155c1119100aa"
+  +  "2f8a5c9053aea6233bd2419c627d92ea72e6728820138e84d82f7b757e1c589f8e03009fb5d660"
+  +  "8a25a5396ad8bc51d00eb880731fd294749d830be0fe1789582cdcc4013c0fa72317a6ef8e44d0"
+  +  "709ffce062f6e187c7295d597d2dc126e50e82a1c96a9ccad863fe3620f62b0ea1651d4403d517"
+  +  "6e89c798")]
+// The AllowPartiallyTrustedCallersAttribute requires the assembly to be signed with a strong name key.
+// This attribute is necessary since the control is called by either an intranet or Internet
+// Web page that should be running under restricted permissions.
+
+//[assembly: FileIOPermission(SecurityAction.RequestMinimum, Unrestricted = true)]
 [assembly: CLSCompliant(true)]
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
