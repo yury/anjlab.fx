8498049e827783bd0008724f6f37a027355f4377
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-07-25 11:18:21 +0000

    add BoolToFontWeightConverter

---
+++ sources/AnjLab.FX.Wpf/Converters/BoolToFontWeightConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    [ValueConversion(typeof(bool), typeof(FontWeight))]
+    public class BoolToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true.Equals(value) ? FontWeights.Bold : FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
