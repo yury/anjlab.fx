3d4b70e59206e405efd552e4bdaaa9810164acc6
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-09-01 15:33:14 +0000

    add nullable type converter

---
+++ sources/AnjLab.FX.Wpf/Converters/NullableTypeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    public class NullableTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(int?)) return System.Convert.ToInt32(value);
+            if (targetType == typeof(short?)) return System.Convert.ToInt16(value);
+            if (targetType == typeof(long?)) return System.Convert.ToInt64(value);
+            if (targetType == typeof(decimal?)) return System.Convert.ToDecimal(value);
+           
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(int?)) return System.Convert.ToInt32(value);
+            if (targetType == typeof(short?)) return System.Convert.ToInt16(value);
+            if (targetType == typeof(long?)) return System.Convert.ToInt64(value);
+            if (targetType == typeof(decimal?)) return System.Convert.ToDecimal(value);
+
+            return value;
+        }
+    }
+}
