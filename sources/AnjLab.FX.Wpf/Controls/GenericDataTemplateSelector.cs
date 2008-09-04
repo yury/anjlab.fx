55392415393a98e7ce1b868c1da87f31e2e1233d
Nikolay Zhebrun <nikolay.zhebrun@gmail.com>
2008-09-04 10:56:22 +0000

    add GenericDataTemplateSelector

---
+++ sources/AnjLab.FX.Wpf/Controls/GenericDataTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using AnjLab.FX.Sys;
+
+namespace AnjLab.FX.Wpf.Controls
+{
+    public class GenericDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplateSelectorItem[] DataTemplateSelectorItems { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+        {
+            if (item != null && DataTemplateSelectorItems != null)
+            {
+                foreach(var selectorItem in DataTemplateSelectorItems)
+                {
+                    var value = Reflector.GetValue(item, selectorItem.PropertyName);
+                    if (selectorItem.Value.Equals(Convert.ToString(value)))
+                        return selectorItem.Template;
+                }
+            }
+
+            return null;
+
+        }
+    }
+
+    public class DataTemplateSelectorItem
+    {
+        public string PropertyName { get; set; }
+        public string Value { get; set; }
+        public DataTemplate Template { get; set; } 
+    }
+}
