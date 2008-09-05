d592265bd0e53bbf6ec3e4957ec0152e66e6a540
Sergey Glukhov <sergey.glukhov@gmail.com>
2008-09-05 15:11:14 +0000

    Forms editor. initial commit.

---
+++ sources/AnjLab.FX.Wpf/Converters/BindingExpressionConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    public class BindingExpressionConverter : ExpressionConverter
+    {
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            if (destinationType == typeof(MarkupExtension))
+                return true;
+            return false;
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(MarkupExtension))
+            {
+                var bindingExpression = value as BindingExpression;
+                if (bindingExpression == null)
+                    throw new Exception();
+                return bindingExpression.ParentBinding;
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+
+        public static void RegisterExpression<T>()
+        {
+            var attribute = new Attribute[1];
+            var vConv = new TypeConverterAttribute(typeof(BindingExpressionConverter));
+            attribute[0] = vConv;
+            TypeDescriptor.AddAttributes(typeof(T), attribute);
+        }
+    }
+}
