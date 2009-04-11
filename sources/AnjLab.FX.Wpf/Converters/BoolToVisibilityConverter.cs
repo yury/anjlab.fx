0937bdb01141d27702bef95d31c35d70ebd216b9
Sergey A. Glukhov <sergey.glukhov@gmail.com>
2009-04-11 15:30:34 +0400

    BoolToVisibilityConverter class added

---
+++ sources/AnjLab.FX.Wpf/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnjLab.FX.Wpf.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true.Equals(value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
