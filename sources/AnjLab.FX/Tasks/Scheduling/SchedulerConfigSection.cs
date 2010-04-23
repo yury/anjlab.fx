eaa3fc316779960bd1ffb2ee846153c3930cb79f
unknown <dmitrygusev@@gmail.com>
2010-04-23 15:59:15 +0400

    SchedulerConfigSection to schedule trigger using web/app.config

---
+++ sources/AnjLab.FX/Tasks/Scheduling/SchedulerConfigSection.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Xml;
+
+namespace AnjLab.FX.Tasks.Scheduling
+{
+    public class SchedulerConfigSection : IConfigurationSectionHandler
+    {
+        #region IConfigurationSectionHandler Members
+
+        object IConfigurationSectionHandler.Create(
+          object parent, object configContext, XmlNode section)
+        {
+            return Trigger.ReadTriggers(section.OuterXml);
+        }
+
+        #endregion
+    }
+
+}
