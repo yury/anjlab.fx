376525fd675863d22273c62208edc1465de80a2a
sergey.glukhov <sergey.glukhov@gmail.com>
2010-07-05 13:08:21 +0400

    allow setters for Sys.EventArgs Items

--- sources/AnjLab.FX/Sys/EventArgs.cs
+++ sources/AnjLab.FX/Sys/EventArgs.cs
@@ -4,21 +4,16 @@ namespace AnjLab.FX.Sys
 {
     public class EventArgs<T> : EventArgs
     {
-        private readonly T _item;
-
         internal EventArgs(T item)
         {
-            _item = item;
+            Item = item;
         }
 
         protected EventArgs()
         {
         }
 
-        public T Item
-        {
-            get { return _item; }
-        }
+        public T Item { get; set; }
 
         static readonly EventArgs<T> _empty = new EventArgs<T>();
         public static new EventArgs<T> Empty { get { return _empty; } }
@@ -26,28 +21,19 @@ namespace AnjLab.FX.Sys
 
     public class EventArgs<T1, T2> : EventArgs
     {
-        private readonly T1 _item1;
-        private readonly T2 _item2;
-
         internal EventArgs(T1 item1, T2 item2)
         {
-            _item1 = item1;
-            _item2 = item2;
+            Item1 = item1;
+            Item2 = item2;
         }
 
         protected EventArgs()
         {
         }
 
-        public T1 Item1
-        {
-            get { return _item1; }
-        }
+        public T1 Item1 { get; set; }
 
-        public T2 Item2
-        {
-            get { return _item2; }
-        }
+        public T2 Item2 { get; set; }
 
         static readonly EventArgs<T1,T2> _empty = new EventArgs<T1,T2>();
         public static new EventArgs<T1,T2> Empty { get { return _empty; } }
@@ -55,35 +41,22 @@ namespace AnjLab.FX.Sys
 
     public class EventArgs<T1, T2, T3> : EventArgs
     {
-        private readonly T1 _item1;
-        private readonly T2 _item2;
-        private readonly T3 _item3;
-
         internal EventArgs(T1 item1, T2 item2, T3 item3)
         {
-            _item1 = item1;
-            _item2 = item2;
-            _item3 = item3;
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
         }
 
         protected EventArgs()
         {
         }
 
-        public T1 Item1
-        {
-            get { return _item1; }
-        }
+        public T1 Item1 { get; set; }
 
-        public T2 Item2
-        {
-            get { return _item2; }
-        }
+        public T2 Item2 { get; set; }
 
-        public T3 Item3
-        {
-            get { return _item3; }
-        }
+        public T3 Item3 { get; set; }
 
         static readonly EventArgs<T1, T2, T3> _empty = new EventArgs<T1, T2, T3>();
         public static new EventArgs<T1, T2, T3> Empty { get { return _empty; } }
